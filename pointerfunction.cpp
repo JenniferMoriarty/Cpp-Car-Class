d04b09d97d37e99bce1bcae2f2cdb8fea59e98b8
Jen Moriarty <JMoriarty78@gmail.com>
2025-02-24 08:29:20 -0700

    Create pointerfunction.cpp

---
+++ pointerfunction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function to increment the values of two integers by 1
+void increment(int *x, int *y) {
+    *x = *x + 1;  // Increment the value pointed to by x
+    *y = *y + 1;  // Increment the value pointed to by y
+}
+
+int main() {
+    int a = 5;
+    int b = 8;
+    
+    cout << "Original values:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    
+    // Calling the increment function
+    increment(&a, &b);
+    
+    cout << "Values after incrementing:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    
+    return 0;
+}
